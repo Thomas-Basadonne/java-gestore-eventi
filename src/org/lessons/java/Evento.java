40c1d3eb95ddb95614267bd4402b2ecb41e5e039
Thomas-Basadonne <thommybasa@gmail.com>
2023-06-13 10:56:25 +0200

    costruttore + getter e setter

---
+++ src/org/lessons/java/Evento.java
@@ -0,0 +1,55 @@
+package org.lessons.java;
+
+import java.time.LocalDate;
+
+public class Evento {
+    private String titolo;
+    private LocalDate data;
+    private final int postiTotali;
+    private int postiPrenotati;
+
+    //costruttore
+    public Evento(String titolo, LocalDate data, int postiTotali) throws IllegalArgumentException {
+        if (data.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("La data dell'evento non può essere nel passato.");
+        }
+        if (postiTotali <= 0) {
+            throw new IllegalArgumentException("Il numero di posti totali deve essere positivo.");
+        }
+
+        this.titolo = titolo;
+        this.data = data;
+        this.postiTotali = postiTotali;
+        this.postiPrenotati = 0;
+    }
+
+    //GETTER E SETTER
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) throws IllegalArgumentException {
+        if (data.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("La data dell'evento non può essere nel passato.");
+        }
+        this.data = data;
+    }
+
+    public int getPostiTotali() {
+        return postiTotali;
+    }
+
+    public int getPostiPrenotati() {
+        return postiPrenotati;
+    }
+
+
+}
